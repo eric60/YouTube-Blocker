0ee857c9df3969bb8ce6d161ab693ea28ef0978c
Eric Shi <ericshi98@yahoo.com>
2019-12-28 19:30:14 -0500

    Moved logic to contentScript and created env variables.

--- js/background.js
+++ js/background.js
@@ -1,48 +1,53 @@
-let currentURL;
-let API_KEY = "AIzaSyAeebo7DlkB6YyCem51Lq9AOAmFG1Nbkxg";
-let allowedIds = ['26', '27', '28']; // video category ids for Howto & Style, Education, Science & technology
 let activated;
 
-(function() {
-  var queryInfo = {
-    'active': true, 
-    'lastFocusedWindow': true,
-    'windowId': chrome.windows.WINDOW_ID_CURRENT
-  }
-  chrome.tabs.query(queryInfo,
-   function(tabs){
-      currentURL = tabs[0].url;
-   }
-  );
-})();
+// (function() {
+//   var queryInfo = {
+//     'active': true, 
+//     'lastFocusedWindow': true,
+//     'windowId': chrome.windows.WINDOW_ID_CURRENT
+//   }
+//   chrome.tabs.query(queryInfo,
+//    function(tabs){
+//       let currentURL = tabs[0].url;
+//       console.log(currentURL)
+//    }
+//   );
+// })();
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab.url :
+//                 "from the extension");
+//     currentURL = sender.tab.url;
+
+//     if (request.initiate) {
+//       initiateisAllowed(currentURL)
+//       sendResponse({initiated: true});
+//     }
+    
+// });
 
 $(document).ready(function(){
 
-  chrome.runtime.onInstalled.addListener(function(){
-    console.log("App Installed");
-    getActivated(executeContentScript);
-  });
+  // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  //   if(changeInfo.status == 'complete') {
+  //     let isAllowed = isAllowed(currentURL)
 
-  chrome.tabs.onCreated.addListener((tab) => {
-    getActivated(executeContentScript);
-  }) 
+  //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  //       chrome.tabs.sendMessage(tabs[0].id, {isAllowed: isAllowed}, function(response) {
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if(changeInfo.status == 'complete') {
-      executeContentScript()
-    }
+  //       });  
+  //     });
 
-  }) 
+  //   }
+
+  // }) 
 
   // Determine Activated status and set button UI
-  getActivated(executeContentScript);
+  getActivated();
 
-  // Determine if URL allowed
-  console.log('current url: ' + currentURL);
-  isAllowed(currentURL);
-
-
-  function getActivated(callback) {
+  function getActivated() {
     chrome.storage.local.get('activated', function(data) {
         if(data.activated === undefined) {
           activated = false
@@ -55,8 +60,6 @@ $(document).ready(function(){
           } else {
             $('#startButton').css('background-color','#f44336')
             $('#startButton').text("Deactivate")
-            console.log("calling callback")
-            callback();
           }
           activated = data.activated;
         }
@@ -71,12 +74,14 @@ $(document).ready(function(){
       }
    })
   }
+
+
  
-  function executeContentScript() {
-    chrome.tabs.executeScript({
-      file: 'js/contentScript.js'
-    });
-  }
+  // function executeContentScript() {
+  //   chrome.tabs.executeScript({
+  //     file: 'js/contentScript.js'
+  //   });
+  // }
 
   $('#startButton').on("click", function() {
     activated = !activated
@@ -89,51 +94,12 @@ $(document).ready(function(){
       $(this).text("Activate")
     }
     setActivated(activated)
-    executeContentScript();
   });
 
 
 
-  function parseToId(url){
-    var regEx = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
-    var match = url.match(regEx);
+ 
 
-    // check match array not null first, returns string id of YouTube video
-    return (match && match[2].length == 11) ? match[2] : null; 
-  }
-
-
-  async function isAllowed(url) {
-    var videoId = parseToId(url);
-    console.log('Youtube video id:' + videoId)
-    if(videoId == null){
-        setAllowed(true)
-       return true; // If URL is NOT a Youtube video then return true
-    }
-    const restAPI = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${API_KEY}&fields=items(snippet(categoryId))&part=snippet`
-    
-    // fetch method to return if current page's youtube video is allowed or not through Youtube's Data API
-     const response = await fetch(restAPI)
-     const json = await response.json()
-     console.log(JSON.stringify(json))
-
-     return processYoutubeData(json)
-  }
-
-  function processYoutubeData(json) {
-    let videoCategory = json.items[0].snippet.categoryId;
-    console.log("The video category of the Youtube video is " + videoCategory);
-
-    let processResult = allowedIds.includes(videoCategory);
-    setAllowed(processResult)
-    
-    console.log('isAllowedUrl: ' + processResult);
-    return processResult
-  }
-
-  function setAllowed(value) {
-    chrome.storage.local.set({allowed : value}, function(){
-    })
-  }
+ 
 
 });
--- js/contentScript.js
+++ js/contentScript.js
@@ -1,12 +1,73 @@
+console.log("initiating Youtube Scholar");
+let currentURL = location.href;
+let allowedIds = ['26', '27', '28']; // video category ids for Howto & Style, Education, Science & technology
 
-chrome.storage.local.get(['activated', 'allowed'], function(data) {
-    console.log('content script allowed' + data.allowed)
+console.log(location.href)
+//console.log(document.getElementsByClassName("yt-simple-endpoint style-scope yt-formatted-string"))
 
-    if (data.activated === true && data.allowed == false) {  
-        alert("Bad youtube video")
-        //location.replace('http://youtube.com')
+// let allowed;
+// chrome.runtime.onMessage.addListener( (message, sender, sendResponse) => { 
+//     isAllowed = message.isAllowed;
+// });
+
+// function setAllowed(value) {
+//     chrome.storage.local.set({allowed : value}, function(){
+//     })
+// }
+
+ initiateisAllowed(currentURL)
+    .then(json => {
+        processYoutubeData(json, finishBlockingYoutubeUrl)
+    })
+
+  function parseToId(url){
+    var regEx = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+    var match = url.match(regEx);
+
+    // check match array not null first, returns string id of YouTube video
+    return (match && match[2].length == 11) ? match[2] : null; 
+  }
+
+  async function initiateisAllowed(url) {
+    let videoId = parseToId(url);
+    console.log('Youtube video id:' + videoId)
+
+    if(videoId == null){
+       return; // If URL is NOT a Youtube video then return true
     }
-});
+    const restAPI = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${API_KEY}&fields=items(snippet(categoryId))&part=snippet`
+
+    let response = await fetch(restAPI)
+    console.log(response)
+    let json = await response.json();
+    console.log(JSON.stringify(json))
+    return json;
+  }
+
+  function processYoutubeData(json, callback) {
+    let videoCategory = json.items[0].snippet.categoryId;
+    console.log("The video category of the Youtube video is " + videoCategory);
+
+    let isAllowedResult = allowedIds.includes(videoCategory);
+
+    console.log('isAllowedUrl: ' + isAllowedResult);
+    if(isAllowedResult) {
+        callback();
+    }
+  }
+
+
+function finishBlockingYoutubeUrl() {
+    chrome.storage.local.get(['activated'], function(data) {
+        console.log('In content script- activated value: ' + data.activated)
+    
+        if (data.activated === true) {  
+            alert("You are watching a non educational youtube video. Redirecting you back to youtube")
+            //location.replace('http://youtube.com')
+        }
+    });
+}
+
 
 
 
