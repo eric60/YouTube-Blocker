a8a05a25a5f4756b998c0a2a67bf2bcfa5877055
Eric S <ericshi98@gmail.com>
2021-10-11 16:06:00 -0400

    Refactor checkUrl function

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,4 +1,4 @@
-let previousSourceVideoData = [];
+let previousvideoMetaData = [];
 let activated;
 const allowedVideoCategories = ["Howto & Style", "Education", "Science & Technology"]
 
@@ -41,30 +41,52 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
  }
 
 function checkUrl(){
-    console.log("inside checkUrl")
-    var scripts = document.scripts;
+    console.log("--- inside checkUrl ---")
+    let scripts = document.scripts;
+    console.log(scripts)
     for(index in scripts) {
         if(typeof scripts[index].innerHTML !== "undefined" && scripts[index].innerHTML.indexOf('"category":') !== -1) {
-            pos = scripts[index].innerHTML.indexOf('"category":');
-            videoMetadata = scripts[index].innerHTML.substring(pos+12,pos+100);
-            console.log("videoMetadata: " + videoMetadata)
-            pos = videoMetadata.indexOf('"');
-            videoCategory = videoMetadata.substring(0,pos);
-            console.log('Youtube Video category is: ' + videoCategory)
-            handleYoutubeBlocking(videoCategory)
-            break;
+            if (handleVideoMetaData(scripts, '"category":', 12, 100)) {
+                break;
+            }
         }
-
         if(typeof scripts[index].innerHTML !== "undefined" && scripts[index].innerHTML.indexOf('"genre":') !== -1) {
-            pos = scripts[index].innerHTML.indexOf('"genre":');
-            videoMetadata = scripts[index].innerHTML.substring(pos+9,pos+100);
-            console.log("videoMetadata: " + videoMetadata)
-            pos = videoMetadata.indexOf('"');
-            videoCategory = videoMetadata.substring(0,pos);
-            handleYoutubeBlocking(videoCategory)
-            break;
+            if (handleVideoMetaData(scripts, '"genre":', 9, 100)) {
+                break;
+            }
         }
     }
+    // handleNoWayToCheckVideoCategoryError()
+}
+
+function handleNoWayToCheckVideoCategoryError() {
+    alert("Error: Youtube Blocker could not detect the video category of this video.")
+}
+
+function handleVideoMetaData(scripts, videoMetadataHtmlStartStr, startOffset, endOffset) {
+    pos = scripts[index].innerHTML.indexOf(videoMetadataHtmlStartStr);
+    videoMetadata = scripts[index].innerHTML.substring(pos + startOffset, pos + endOffset);
+    if (isVideoMetadataDuplicate(videoMetadata)) {
+        return false;
+    }
+    console.log(`videoMetadata for ${videoMetadataHtmlStartStr}: ` + videoMetadata)
+    pos = videoMetadata.indexOf('"');
+    videoCategory = videoMetadata.substring(0,pos);
+    videoCategory = videoCategory.replace("\\u0026", "&")
+    console.log('Youtube Video category is: ' + videoCategory)
+    handleYoutubeBlocking(videoCategory)
+    return true;
+}
+
+function isVideoMetadataDuplicate(videoMetadata) {
+    console.log("previousvideoMetaData: " + previousvideoMetaData)
+    if (previousvideoMetaData.includes(videoMetadata)) {
+        console.log("video meta data is duplicate")
+        return true;
+    }
+    console.log("video meta data is NOT duplicate")
+    previousvideoMetaData.push(videoMetadata);
+    return false;
 }
 
 function handleYoutubeBlocking(videoCategoryString) {
@@ -83,8 +105,7 @@ function blockYoutubeVideo(videoCategoryString) {
     
         if (data.activated === true) {  
             console.log('---- Sent create notification to background ----');
-            // Block redirecting happens here
-            location.replace('http://youtube.com')
+            //location.replace('http://youtube.com')
             chrome.runtime.sendMessage({createNotification: true, videoCategoryString: videoCategoryString}, 
                 function(response) {
             });     
